f551aad59d48abab21b74fd3de7810284139f64f
Neto test <anna.shuliko@bk.ru>
2023-05-01 19:24:07 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 98;
+        double height = 1.87;
+        double bmi = service.calculate(weight, height);
+        String bmiResult = String.format("%.0f", bmi);
+        System.out.println("Ваши Вес : " + weight + " кг");
+        System.out.println("Ваш Рост : " + height + " м");
+        System.out.println("Индекс массы тела " + bmiResult + " кг/см2");
+
+        }
 }
